88b0ecef4c73ee0483b21412188fceb903d852ad
Travis Smith <travissmithoh@yahoo.com>
2018-06-13 11:05:34 -0400

    Added user area into data

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensedPage from '../components/AddExpensedPage';
 import EditExpensedPage from '../components/EditExpensedPage';
-import HelpPage from '../components/HelpPage';
 import NotFountPage from '../components/NotFountPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
                 <PrivateRoute path="/create" component={AddExpensedPage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensedPage} />
-                <Route path="/help" component={HelpPage} />
                 <Route component={NotFountPage} />
             </Switch>
         </div>

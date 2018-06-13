60cca7a50d8a4f47ff68172f417334766f7fc55a
Travis Smith <travissmithoh@yahoo.com>
2018-06-13 15:46:47 -0400

    added styling

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { login, logout } from './actions/auth';
 import './styles/styles.scss'
 import 'normalize.css/normalize.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -25,7 +26,7 @@ const renderApp = () => {
         hasRendered = true;
     }
 };
-ReactDOM.render(<p>loading...</p> , document.getElementById('app'));
+ReactDOM.render(<LoadingPage /> , document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import numberal from 'numeral';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-    const formattedExpensesTotal = numberal(expensesTotal / 100).format('$0,0.00');
-    return (
-    <div>
-        <h1>
-            Viewing {expenseCount} {expenseWord} with a total of {formattedExpensesTotal}
-        </h1>
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} with a total of <span>{formattedExpensesTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 const mapStateToProps = (state) => {
-    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
 
-    return {
-        expenseCount: visibleExpenses.length,
-        expensesTotal: selectExpensesTotal(visibleExpenses)
-    }
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  };
 };
 
 export default connect(mapStateToProps)(ExpensesSummary);
